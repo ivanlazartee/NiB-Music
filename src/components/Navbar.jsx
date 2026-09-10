e4cf3e1da3aff4c9a205d744d831bd860e76183c
BenjaFedev <bfm7474@gmail.com>
2026-09-09 21:18:36 -0300

    feat: mejora navegación lateral y navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,23 @@
+import { Link } from "react-router-dom";
+import { Search } from "lucide-react";
+
 const Navbar = () => {
   return (
     <header className="navbar">
       <div className="navbar__search">
+        <Search className="navbar__search-icon" size={19} />
+
         <input
           type="text"
-          placeholder="Buscar canciones, artistas..."
+          placeholder="Buscar canciones, artistas, álbumes..."
           className="navbar__input"
         />
       </div>
 
       <div className="navbar__actions">
-        <button className="navbar__button">
+        <Link to="/login" className="navbar__button">
           Iniciar sesión
-        </button>
+        </Link>
       </div>
     </header>
   );
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,18 @@
 import { NavLink } from "react-router-dom";
+import {
+  House,
+  Search,
+  Library,
+  Heart,
+  ListMusic,
+  User,
+} from "lucide-react";
+
 import logoNib from "../assets/img/Nib-Home.png";
 
 const Sidebar = () => {
   return (
     <aside className="sidebar">
-
-      {/* Logo NiB Music */}
       <NavLink to="/" className="sidebar__brand-link">
         <img
           src={logoNib}
@@ -14,25 +21,63 @@ const Sidebar = () => {
         />
       </NavLink>
 
-      {/* Navegación */}
       <nav className="sidebar__nav">
         <NavLink to="/" end className="sidebar__link">
-          Inicio
+          <House size={20} />
+          <span>Inicio</span>
         </NavLink>
 
         <NavLink to="/catalogo" className="sidebar__link">
-          Explorar
+          <Search size={20} />
+          <span>Explorar</span>
         </NavLink>
 
         <NavLink to="/playlist" className="sidebar__link">
-          Playlists
-        </NavLink>
-
-        <NavLink to="/perfil" className="sidebar__link">
-          Perfil
+          <Library size={20} />
+          <span>Biblioteca</span>
         </NavLink>
       </nav>
 
+      <div className="sidebar__section">
+        <p className="sidebar__section-title">PLAYLISTS</p>
+
+        <NavLink to="/playlist" className="sidebar__playlist-link">
+          <Heart size={18} />
+          <span>Tus Me Gusta</span>
+        </NavLink>
+
+        <NavLink to="/playlist" className="sidebar__playlist-link">
+          <ListMusic size={18} />
+          <span>Éxitos 2024</span>
+        </NavLink>
+
+        <NavLink to="/playlist" className="sidebar__playlist-link">
+          <ListMusic size={18} />
+          <span>Lo Fi Chill</span>
+        </NavLink>
+
+        <NavLink to="/playlist" className="sidebar__playlist-link">
+          <ListMusic size={18} />
+          <span>Rock Classics</span>
+        </NavLink>
+
+        <NavLink to="/playlist" className="sidebar__playlist-link">
+          <ListMusic size={18} />
+          <span>Gym Mode</span>
+        </NavLink>
+
+        <NavLink to="/playlist" className="sidebar__playlist-link">
+          <ListMusic size={18} />
+          <span>Para Programar</span>
+        </NavLink>
+      </div>
+
+      <div className="sidebar__profile">
+        <NavLink to="/perfil" className="sidebar__link">
+          <User size={20} />
+          <span>Perfil</span>
+        </NavLink>
+      </div>
     </aside>
   );
 };
