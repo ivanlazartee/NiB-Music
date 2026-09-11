a2bf43b6f063473b5d392f2494e5963d14c88e37
nicoolas-devel <nicoolas23@gmail.com>
2026-09-11 01:37:15 -0300

    feat: agregar vinilo animado a la página 404

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,11 +1,18 @@
 import "./Error404.css";
+import vinilo from "../assets/img/vinilo.png";
 import { Link } from "react-router-dom";
 
 function Error404() {
   return (
     <main className="error404-container">
       <section className="error404-content">
-        <div className="error404-image"></div>
+        <div className="error404-image">
+        <img
+              src={vinilo}
+              alt="Disco de vinilo"
+              className="vinilo"
+        />
+</div>
 
         <div className="error404-info">
           <h1>404</h1>
