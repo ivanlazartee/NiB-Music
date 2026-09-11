527f809ecabd7ea32faf91c51ef015044f6e0fa7
nicoolas-devel <nicoolas23@gmail.com>
2026-09-11 01:05:50 -0300

    style: agregar estilos base de la página 404

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,8 +1,30 @@
+import "./Error404.css";
+import { Link } from "react-router-dom";
+
 function Error404() {
   return (
-    <section>
-      <h1>Error 404</h1>
-    </section>
+    <main className="error404-container">
+      <section className="error404-content">
+        <div className="error404-image"></div>
+
+        <div className="error404-info">
+          <h1>404</h1>
+
+          <h2>Esta canción ya no está en la playlist</h2>
+
+          <p>
+            Puede que la página haya sido eliminada, movida o que el enlace sea
+            incorrecto.
+          </p>
+
+          <div className="error404-buttons">
+            <Link to="/">Volver al inicio</Link>
+
+            <Link to="/">Explorar música</Link>
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }
 
