d3d3139b64dfbba9683a85098535ca58934310aa
nicoolas-devel <nicoolas23@gmail.com>
2026-09-16 15:30:20 -0300

    Agrego la estructura inicial del login y el navbar

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,38 @@
-function Login () {
-    return <h1>Login</h1>
+import loginBg from "../assets/img/login-bg.png";
+import "./login.css";
+
+function Login() {
+  return (
+    <section className="login-page">
+
+      <img
+        src={loginBg}
+        alt="NiB Music"
+        className="login-bg"
+      />
+
+      <div className="login-overlay">
+
+        {/* Navbar */}
+        <nav className="login-navbar">
+        <a href="/">Inicio</a>
+        <a href="/catalogo">Explorar</a>
+        <a href="#">Soporte</a>
+
+<button className="login-user">
+  👤
+</button>
+        </nav>
+
+        {/* Modal */}
+        <div className="login-card">
+
+        </div>
+
+      </div>
+
+    </section>
+  );
 }
 
-export default Login
+export default Login;
