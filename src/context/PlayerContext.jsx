a103dcd128d4abc3427625e69e2d5753843e136b
Ivan A. Lazarte <lazarteivan18@gmail.com>
2026-09-19 02:56:06 -0300

    correcion de PlayerContext

--- src/context/PlayerContext.jsx
+++ src/context/PlayerContext.jsx
@@ -1,10 +1,30 @@
 import { createContext, useContext, useState, useRef } from 'react'
+
 export const PlayerContext = createContext(null)
+
 export function usePlayer() {
   return useContext(PlayerContext)
 }
+
 export function PlayerProvider({ children }) {
   const [cancionActual, setCancionActual] = useState(null)
   const [reproduciendo, setReproduciendo] = useState(false)
   const [cola, setCola] = useState([])
   const audioRef = useRef(new Audio())
+
+  return (
+    <PlayerContext.Provider
+      value={{
+        cancionActual,
+        reproduciendo,
+        cola,git
+        setCancionActual,
+        setReproduciendo,
+        setCola,
+        audioRef,
+      }}
+    >
+      {children}
+    </PlayerContext.Provider>
+  )
+}
