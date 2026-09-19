c669c3fbbcf1c665862cd6b67efc70530fbfc538
nicoolas-devel <nicoolas23@gmail.com>
2026-09-19 03:40:32 -0300

    feat: integrar usuarios premium en panel admin

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,4 +1,11 @@
-function Admin (){
-    return <h1>Admin</h1>
+import UsuariosAdmin from "../components/admin/UsuariosAdmin";
+
+function Admin() {
+  return (
+    <main>
+      <UsuariosAdmin />
+    </main>
+  );
 }
-export default Admin
+
+export default Admin;
